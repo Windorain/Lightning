03e447cf531406434b40e8bc53a41db199aaed94
称明 <8456918+Windorain@users.noreply.github.com>
2026-05-13 22:53:25 +0800

    chore: remove old ToolShelf.vue and useSnapping composable (replaced by PanelDeclaration)

--- web/src/workbench/composables/useSnapping.ts
+++
@@ -1,9 +0,0 @@
-// web/src/workbench/composables/useSnapping.ts
-import { ref } from 'vue'
-
-const snapEnabled = ref(true)
-
-export function useSnapping() {
-  function toggleSnap(): void { snapEnabled.value = !snapEnabled.value }
-  return { snapEnabled, toggleSnap }
-}
